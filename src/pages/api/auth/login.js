d0478bdf61943f203d168cc63e2685b33548bbf6
masha <mariyapnmrv@gmail.com>
2024-03-07 19:05:41 -0600

    Added structure

---
+++ src/pages/api/auth/login.js
@@ -0,0 +1,32 @@
+import dbConnect from "@/lib/utils";
+import { User } from "@/lib/models";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { email, password } = req.body;
+  await dbConnect();
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "User not found, login attempt failed" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res
+      .status(401)
+      .json({ message: "Wrong credentials, login attempt failed" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "2h",
+  });
+  res.status(200).json({ token });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "custom-heading": ["Luckiest Guy", "sans-serif"],
+        "custom-paragraph": ["Quicksand", "sans-serif"],
+        "custom-extra": ["Indie Flower", "sans-serif"],
+      },
+      colors: {
+        "cherry-main": "#F94144",
+        "bright-tangerine": "#F3722C",
+        "retro-yellow": "#F9C74F",
+        "yellow-main": "#FFC12B",
+        "retro-green": "#90BE6D",
+        "darkest-custom": "#222020",
+        "darker-custom": "#373535",
+        "dark-custom": "#454343",
+        "cream-custom": "#FFF6DF",
+        "cold-gray": "#E8E8E8",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
