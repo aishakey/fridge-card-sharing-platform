fe2ac256ff050a3b89bee65b1e6eacf78843b95e
masha <mariyapnmrv@gmail.com>
2024-03-21 14:47:45 -0500

    my fridge page done

--- src/app/my-fridge/page.jsx
+++ src/app/my-fridge/page.jsx
@@ -1,5 +1,44 @@
-const MyFridge = () => {
-  return <div>MyFridge</div>;
-};
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
-export default MyFridge;
+export default async function MyFridgePage() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/");
+  }
+  return (
+    <div className="flex flex-col mt-6 md:mt-4 md:flex-row md:justify-around items-center px-10">
+      <div className="flex flex-col gap-6 md:gap-14 mb-12 md:mb-0 items-center w-full md:order-2">
+        <Link
+          href="/my-cards"
+          className="w-3/5 py-2 md:py-3 md:w-1/2 custom-button"
+        >
+          Delivered Cards
+        </Link>
+        <Link
+          href="/send-card"
+          className="w-3/5 py-2 md:py-3 md:w-1/2 custom-button"
+        >
+          Send Card
+        </Link>
+        <Link
+          href="/my-cards"
+          className="w-3/5 py-2 md:py-3 md:w-1/2 custom-button"
+        >
+          Received Cards
+        </Link>
+      </div>
+      <div className="flex justify-center w-full md:order-1">
+        <Image
+          src="/empty-fridge.svg"
+          alt="Green fridge"
+          width={380}
+          height={200}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,53 +8,52 @@ import { useSession, signOut } from "next-auth/react";
 export default function Navbar() {
   const { data: session, status: sessionStatus } = useSession();
   const isAuthenticated = sessionStatus === "authenticated";
-  console.log("status", sessionStatus);
-  console.log("session", session);
 
   const pathname = usePathname();
 
   const hideOnRoutes = ["/login", "/register"];
+  const specialColorRoutes = ["/contact", "/send-card"];
 
   if (hideOnRoutes.includes(pathname)) {
     return null;
   }
+
+  const isSpecialColorRoute = specialColorRoutes.includes(pathname);
+
+  const logoImageSrc = isSpecialColorRoute ? "/brand-white.svg" : "/brand.svg";
+
+  const navbarClasses = isSpecialColorRoute
+    ? "flex items-center justify-between px-8 py-4 bg-darkest-custom"
+    : "flex items-center justify-between px-8 py-4"; // Default styles
+
+  const linkClasses = isSpecialColorRoute
+    ? "text-cream-custom hover:text-gray-300 font-bold text-lg"
+    : "text-darkest-custom hover:text-gray-600 font-bold text-lg"; //Default styles
 
   if (sessionStatus === "loading") {
     return <div>Loading...</div>; // switch to loader
   }
 
   return (
-    <nav className="flex items-center justify-between px-8 py-4">
+    <nav className={navbarClasses}>
       <Link href="/" passHref>
-        <Image src="/brand.svg" alt="fridge" width={100} height={50} />
+        <Image src={logoImageSrc} alt="fridge" width={100} height={50} />
       </Link>
 
       {/* Navigation Links */}
       <div className="flex items-center gap-14">
-        <Link
-          href="/about"
-          className="text-darkest-custom hover:text-gray-600 font-bold text-lg"
-        >
+        <Link href="/about" className={linkClasses}>
           About
         </Link>
-        <Link
-          href="/contact"
-          className="text-darkest-custom hover:text-gray-600 font-bold text-lg"
-        >
+        <Link href="/contact" className={linkClasses}>
           Contact
         </Link>
         {isAuthenticated ? (
-          <Link
-            href="/my-profile"
-            className="text-darkest-custom hover:text-gray-600 font-bold text-lg"
-          >
+          <Link href="/my-profile" className={linkClasses}>
             My Profile
           </Link>
         ) : (
-          <Link
-            href="/register"
-            className="text-darkest-custom hover:text-gray-600 font-bold text-lg"
-          >
+          <Link href="/register" className={linkClasses}>
             Register
           </Link>
         )}
@@ -70,7 +69,7 @@ export default function Navbar() {
             Sign out
           </button>
         ) : (
-          <Link href="/login" className="custom-button text-darkest-custom">
+          <Link href="/login" className="custom-button">
             Sign in
           </Link>
         )}
